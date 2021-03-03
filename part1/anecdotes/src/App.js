7c7f0c1217c7c49a64d7c0f082f120793a6a04f1
L u c i f e r <lucifer@pop-os.localdomain>
2021-03-03 22:38:46 +0700

    adding submission for part1 anecdotes part3

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -2,10 +2,12 @@ import React, { useState } from "react"
 import anecdotes from "./data"
 import Anecdote from "./components/Anecdote"
 import Button from "./components/Button"
+import Title from "./components/Title"
 const App = () => {
   const initVotes = Array(anecdotes.length).fill(0)
 
   const [selected, setSelected] = useState(0)
+  const [mostVoted, setMostVoted] = useState(0)
   const [votes, setVotes] = useState(initVotes)
 
   const getRandomNumber = () => {
@@ -16,14 +18,20 @@ const App = () => {
   const addingVotes = () => {
     const newVotes = [...votes]
     newVotes[selected]++
+    if (newVotes[selected] > newVotes[mostVoted]) {
+      setMostVoted(selected)
+    }
     setVotes(newVotes)
   }
 
   return (
     <>
+      <Title text='anecdotes of the day'></Title>
       <Anecdote text={anecdotes[selected]} votes={votes[selected]}></Anecdote>
       <Button eventHandler={getRandomNumber} text='Next anecdote'></Button>
       <Button eventHandler={addingVotes} text='votes'></Button>
+      <Title text='anecdotes of the most voted'></Title>
+      <Anecdote text={anecdotes[mostVoted]} votes={votes[mostVoted]}></Anecdote>
     </>
   )
 }
